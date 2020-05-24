dfa936dff37fc3fb3568a5765e7403be7e0d8568
nagasee0526 <roronalove@gmail.com>
2020-05-24 12:16:59 +0900

    &&를 이용한 if문의 활용

--- app/src/main/java/com/ubis/loginpreatice/MainActivity.kt
+++ app/src/main/java/com/ubis/loginpreatice/MainActivity.kt
@@ -19,15 +19,24 @@ class MainActivity : AppCompatActivity() {
 //            Log.d("입력아이디",getid)
 //            Log.d("입력 비번",getpass)
 
-            if ( getid == "admin") {
-                Log.d("입력값 확인", "admin임")
+            if ( getid == "admin@admin.com" && getpass == "qwer") {
+                Log.d("로그인확인", "관리자입니다.")
             }
-            else if( getid == "admin@admin.com") {
-                Log.d("입력값 확인", "관리자 이메일임")
+            else    {
+                Log.d("로그인확인", "로그인에 실패했습니다..")
             }
-            else {
-                Log.d("입력값 확인", "틀린 아이디")
-            }
+//            else if( getid == "admin@admin.com") {
+//                Log.d("입력값 확인", "관리자 이메일임")
+//            }
+//            else {
+//                Log.d("입력값 확인", "틀린 아이디")
+//            }
+
+//            when( getid ) {
+//                "admin" -> { Log.d("입력값 확인", "admin임") }
+//                "admin@admin.com" -> { Log.d("입력값 확인", "관리자 이메일임") }
+//                else -> { Log.d("입력값 확인", "틀린 아이디") }
+//            }
         }
     }
 }
